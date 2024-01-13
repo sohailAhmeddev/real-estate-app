daea7a520da30ced349931ec9bbe0dffda2a6d30
fareed murad <sohailahmad.6253@gmail.com>
2024-01-14 00:47:35 +0500

    sigin functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -6,37 +6,75 @@ import { Link, useNavigate } from 'react-router-dom';
 //   signInSuccess,
 //   signInFailure,
 // } from '../redux/user/userSlice';
-import OAuth from '../components/OAuth';
+// import OAuth from '../components/OAuth';
+import { useState } from 'react';
 
 export default function SignIn() {
+
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState('');
+  const [error, setError] = useState('');
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError('');
+      // navigate('/');
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
-      <form  className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='email'
           placeholder='email'
           className='border p-3 rounded-lg'
           id='email'
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <input
           type='password'
           placeholder='password'
           className='border p-3 rounded-lg'
           id='password'
-          // onChange={handleChange}
+          onChange={handleChange}
         />
 
         <button
           disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-          {/* {loading ? 'Loading...' : 'Sign In'} */}
-          Sing in
+          {loading ? 'Loading...' : 'Sign In'}
         </button>
-        <OAuth/>
+        {/* <OAuth /> */}
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Dont have an account?</p>
@@ -44,7 +82,7 @@ export default function SignIn() {
           <span className='text-blue-700'>Sign up</span>
         </Link>
       </div>
-      {/* {error && <p className='text-red-500 mt-5'>{error}</p>} */}
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   );
 }
