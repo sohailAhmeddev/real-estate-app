9c9ab0e8be29c0b88862db2d1aa28ae2c73fd129
fareed murad <sohailahmad.6253@gmail.com>
2024-01-13 19:14:23 +0500

    connected db

--- api/index.js
+++ api/index.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
-// mongoose
-//   .connect("mongodb+srv://sohailahmad:real-estate@real-estate.q7fqhql.mongodb.net/?retryWrites=true&w=majority")
-//   .then(() => {
-//     console.log('Connected to MongoDB!');
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 
 const app = express();
