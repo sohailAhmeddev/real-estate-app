bb4cf3c36d95d8293bad2dba729859cad41d0f2a
fareed murad <sohailahmad.6253@gmail.com>
2024-01-15 21:56:11 +0500

    add redux middleware

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,19 +1,19 @@
-// import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-// import { useDispatch, useSelector } from 'react-redux';
-// import {
-//   signInStart,
-//   signInSuccess,
-//   signInFailure,
-// } from '../redux/user/userSlice';
-// import OAuth from '../components/OAuth';
 import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
+// import OAuth from '../components/OAuth';
 
 export default function SignIn() {
 
   const [formData, setFormData] = useState({});
-  const [loading, setLoading] = useState('');
-  const [error, setError] = useState('');
+  const dispatch = useDispatch();
+  
+  const {loading, error} = useSelector((state) => state.user);
 
   const handleChange = (e) => {
     setFormData({
@@ -25,7 +25,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart())
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -36,16 +36,13 @@ export default function SignIn() {
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message))
         return;
       }
-      setLoading(false);
-      setError('');
-      // navigate('/');
+      dispatch(signInSuccess(data));
+      navigate('/');
     } catch (error) {
-      setError(error);
-      setLoading(false);
+      dispatch(signInFailure(data.message))
     }
   }
 
